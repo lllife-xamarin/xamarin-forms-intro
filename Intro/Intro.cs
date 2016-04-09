0a87a4f3a248d46cbdff782f4b3e4a76c428df00
Somnuk Wongkhan <somnuk.wk@gmail.com>
2016-04-09 14:06:00 +0700

    Initial commit

---
+++ Intro/Intro.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Intro
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			// The root page of your application
+			var content = new ContentPage {
+				Content = new Label {
+					Text = "Hello, Form!",
+					VerticalOptions = LayoutOptions.CenterAndExpand,
+					HorizontalOptions = LayoutOptions.CenterAndExpand
+				}
+			};
+
+			MainPage = new NavigationPage (new StackLayoutExample());
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
---
+++ Intro/StackLayoutExample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Intro
+{
+	public class StackLayoutExample : ContentPage
+	{
+		public StackLayoutExample ()
+		{
+			Padding = new Thickness (20);
+			var red = new Label {
+				Text = "Stop", BackgroundColor = Color.Red, FontSize = 20
+			};
+
+			var yellow = new Label {
+				Text = "Slow down", BackgroundColor = Color.Yellow, FontSize = 20
+			};
+
+			var green = new Label {
+				Text = "Go", BackgroundColor = Color.Green, FontSize = 20
+			};
+
+			Content = new StackLayout { 
+				Spacing = 10,
+				Children = { red, yellow, green }
+			};
+		}
+	}
+}
+
