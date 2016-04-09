5958a7ea7488f98278fd5497fe5cf7e898e08e55
Somnuk Wongkhan <somnuk.wk@gmail.com>
2016-04-09 16:26:12 +0700

    Updated screen

---
+++ Intro/CustomCellXaml.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Intro
+{
+	public partial class CustomCellXaml : ContentPage
+	{
+		public CustomCellXaml ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
